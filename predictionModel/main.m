7c85f7c159ce47ca6498c1270babfd6cdbcdfaf2
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2019-03-23 04:10:51 +0530

    Updated main.m, Added training data

--- predictionModel/main.m
+++ predictionModel/main.m
@@ -2,9 +2,14 @@
 close all;
 clc;
 
-%=====Loading Data=========
+%=====Loading Data==========
 pkg load io
 dat = xlsread ('Travelpac2017.xlsx', 'Numeric 2017');
 % quarter   mode     age      duration   budget
 X=[dat(:,2) dat(:,4) dat(:,8) dat(:,10)];
 y = [dat(:,5)] - 9;
+
+%======Training Data========
+lambda = 0.1;
+num_labels = 83
+[all_theta] = oneVsAll(X, y, num_labels, lambda);
